721bdda7d80629bf4066bbb5d913233570472f76
Matthias Max <matthias.max@bitflower.net>
2022-11-30 22:33:59 +0100

    .

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,35 @@
 import { getApi } from './make-api';
 
 const runMe = async () => {
-  //   bla({ openApiSpecPath: 'es_swagger.json' });
+  const SPEC_URL = 'https://api.apis.guru/v2/openapi.yaml';
+  let BASE_URL = 'https://api.apis.guru/v2/';
+
+  //   const SPEC_URL = 'es_swagger.json';
+  //   let BASE_URL = 'https://api.apis.guru/v2/';
+
   const apiClient = await getApi({
-    openApiSpecPath: 'https://api.apis.guru/v2/openapi.yaml',
+    openApiSpecPath: SPEC_URL
   });
+
+  if (apiClient.api.getBaseURL()) {
+    BASE_URL = apiClient.api.getBaseURL();
+  }
+  console.log(`BF BASE URL`, apiClient.api.getBaseURL());
 
   //   console.log(`BF PATHS`, apiClient.paths);
 
+  // DIDN'T WORK
   //   apiClient.api.withServer('https://api.apis.guru/v2/');
 
   for (const [path, pathApi] of Object.entries(apiClient.paths)) {
     console.log(`BF PATHAPI for "${path}"`, pathApi);
 
     const { status, statusText, data } = await pathApi.get(null, null, {
-      baseURL: 'https://api.apis.guru/v2/',
+      baseURL: BASE_URL
     });
 
     console.log(`BF RESULT for "${path}".get()`, { status, statusText }, data);
   }
-
-  //   Object.entries(apiClient.paths).forEach(async ([path, pathApi]) => {
-  //     console.log(`BF PATHAPI for "${path}"`, pathApi);
-  //     const result = await pathApi.get(null, null, {
-  //       baseURL: 'https://api.apis.guru/v2/',
-  //     });
-
-  //     console.log(`BF RESULT for "${path}".get()`, result);
-  //   });
 };
 
 runMe();
--- src/make-api.ts
+++ src/make-api.ts
@@ -1,25 +1,15 @@
-import OpenAPIClientAxios from 'openapi-client-axios';
+import OpenAPIClientAxios, { Document } from 'openapi-client-axios';
 
-export const getApi = async ({
-  openApiSpecPath
-}: {
-  openApiSpecPath: string;
-}) => {
+interface GetApiOptions {
+  openApiSpecPath: string | Document;
+}
+
+export const getApi = async ({ openApiSpecPath }: GetApiOptions) => {
   const api = new OpenAPIClientAxios({
     definition: openApiSpecPath
   });
-  //   console.log(`BF API`, { api });
-
-  console.log(`BF BASE URL`, api.getBaseURL());
 
   const client = await api.init();
 
-  //   console.log(`BF CLIENT`, { client });
-  //   console.log(`BF PATHS`, client.paths);
-
   return client;
-
-  //   const result = await client.paths[`/locations`].get();
-
-  //   console.log('Here is pet id:1 from the api', result.data);
 };
